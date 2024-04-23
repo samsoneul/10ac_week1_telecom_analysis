1aa495f07b18fa485c513da368e50c36969ead63
samsoneul <samsonleul2@gmail.com>
2024-04-23 20:58:38 +0300

    loading done

--- src/loader.py
+++ src/loader.py
@@ -1,10 +1,24 @@
-import psycopg2
-import os
-
+import pandas as pd
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
-
+import os
 
 load_dotenv()
 
-pg_user = os.getenv('PG_USER')
-pg_password = os.getenv('PG_PASSWORD')
+user = os.getenv('PG_USER')
+password = os.getenv('PG_PASSWORD')
+
+database_name = 'telecom'
+table_name= 'xdr_data'
+
+connection_params = { "host": "localhost", "user": user, "password": password,
+                    "port": "5432", "database": database_name}
+
+engine = create_engine(f"postgresql+psycopg2://{connection_params['user']}:{connection_params['password']}@{connection_params['host']}:{connection_params['port']}/{connection_params['database']}")
+
+# str or SQLAlchemy Selectable (select or text object)
+sql_query = 'SELECT * FROM xdr_data '
+
+df = pd.read_sql(sql_query, con= engine)
+
+print(df.describe())
