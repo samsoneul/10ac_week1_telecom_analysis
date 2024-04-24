ef089b0ce8989fe77c0ee7d81c306d9a21c867d5
samsoneul <samsonleul2@gmail.com>
2024-04-24 07:22:48 +0300

    task 2.0

--- src/loader.py
+++ src/loader.py
@@ -2,23 +2,24 @@ import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
+def load_data():
+    load_dotenv()
 
-load_dotenv()
+    user = os.getenv('PG_USER')
+    password = os.getenv('PG_PASSWORD')
 
-user = os.getenv('PG_USER')
-password = os.getenv('PG_PASSWORD')
+    database_name = 'telecom'
+    table_name= 'xdr_data'
 
-database_name = 'telecom'
-table_name= 'xdr_data'
+    connection_params = { "host": "localhost", "user": user, "password": password,
+                        "port": "5432", "database": database_name}
 
-connection_params = { "host": "localhost", "user": user, "password": password,
-                    "port": "5432", "database": database_name}
+    engine = create_engine(f"postgresql+psycopg2://{connection_params['user']}:{connection_params['password']}@{connection_params['host']}:{connection_params['port']}/{connection_params['database']}")
 
-engine = create_engine(f"postgresql+psycopg2://{connection_params['user']}:{connection_params['password']}@{connection_params['host']}:{connection_params['port']}/{connection_params['database']}")
+    # str or SQLAlchemy Selectable (select or text object)
+    sql_query = 'SELECT * FROM xdr_data '
 
-# str or SQLAlchemy Selectable (select or text object)
-sql_query = 'SELECT * FROM xdr_data '
+    df = pd.read_sql(sql_query, con= engine)
 
-df = pd.read_sql(sql_query, con= engine)
-
-print(df.describe())
+    
+    return df
---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+
+import os
+
+
+
+def preproccess_numerical_data(df):
+     # Fill missing values with mean for numerical columns
+    numerical_cols = df.select_dtypes(include=['number']).columns
+    df[numerical_cols] = df[numerical_cols].fillna(df[numerical_cols].mean())
+    return df
+
+
+def preproccess_categorical_data(df):
+    categorical_cols = df.select_dtypes(exclude=['number']).columns
+    mode_values = df[categorical_cols].mode().iloc[0]
+    df.loc[:, categorical_cols] = df[categorical_cols].fillna(mode_values)
+    return df
