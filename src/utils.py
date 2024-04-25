c9dcc101f7e1f7ad3578f3b9c9566f53ea7ff3fa
samsoneul <samsonleul2@gmail.com>
2024-04-25 17:15:59 +0300

    task4

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,47 @@
-import pandas as pd 
+import pandas as pd
+from scipy import stats
+import numpy as np
 
-import os
+class DataProcessor:
+    def __init__(self):
+        self.dft = pd.DataFrame()
 
+    def preprocess_numerical_data(self, df):
+        # Fill missing values with mean for numerical columns
+        numerical_columns = df.select_dtypes(include=['float'])
+        for col in numerical_columns:
+            df[col] = df[col].fillna(df[col].mean())
+        return df
 
+    def preproccess_categorical_data(self, df):
+        categorical_cols = df.select_dtypes(exclude=['float']).columns
+        mode_values = df[categorical_cols].mode().iloc[0]
+        df.loc[:, categorical_cols] = df[categorical_cols].fillna(mode_values)
+        return df
 
-def preproccess_numerical_data(df):
-    # Fill missing values with mean for numerical columns
-    numerical_cols = df.select_dtypes(include=['number']).columns
-    for col in numerical_cols:
-        df[col] = df[col].fillna(df[col].mean())
-    return df
-
-
-
-
-
-def preproccess_categorical_data(df):
-    categorical_cols = df.select_dtypes(exclude=['number']).columns
-    mode_values = df[categorical_cols].mode().iloc[0]
-    df.loc[:, categorical_cols] = df[categorical_cols].fillna(mode_values)
-    return df
-
-
+    def remove_outliers_zscore(self, df, z_threshold=3):
+        """
+        Remove outliers from a DataFrame using Z-scores.
+        
+        Parameters:
+        - df: pandas DataFrame
+            Input DataFrame containing numerical columns.
+        - z_threshold: float, optional (default=3)
+            Z-score threshold to identify outliers.
+            
+        """
+        
+        # Select numerical columns
+        numerical_cols = df.select_dtypes(include=['float']).columns
+        
+        # Calculate Z-scores
+        z_scores = stats.zscore(df[numerical_cols])
+        abs_z_scores = np.abs(z_scores)
+        
+        # Create boolean mask to filter outliers
+        filtered_entries = (abs_z_scores < z_threshold).all(axis=1)
+        
+        # Update DataFrame by keeping only non-outlier rows
+        df_clean = df[filtered_entries]
+        
+        return df_clean
